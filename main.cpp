5b087920ac29d9664674b2f314051eb41df0ddab
ManuNunez <manu.nunez.2004@gmail.com>
2024-05-22 11:57:44 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
 #define ss second
 #define ff first
@@ -29,12 +29,77 @@ typedef vector<vi> vvi;
 
 const int MOD = 1000000007;
 const double PI = acos(-1.0);
+const double TOLERANCE = 1e-10;
 
+void multiplyVectorMatrix(const vector<double>& vector, const vector<vector<double>>& matrix, vector<double>& result, int n) {
+    fore(i, n) {
+        result[i] = 0;
+        fore(j, n) {
+            result[i] += vector[j] * matrix[j][i];
+        }
+    }
+}
+
+void normalizeRow(vector<double>& row, int n) {
+    double sum = accumulate(all(row), 0.0);
+    fore(i, n) {
+        row[i] /= sum;
+    }
+}
+
+void findSteadyState(const vector<vector<double>>& P, vector<double>& pi, int n) {
+    vector<double> pi_next(n);
+    double sum_diff;
+
+    do {
+        multiplyVectorMatrix(pi, P, pi_next, n);
+
+        sum_diff = 0;
+        fore(i, n) {
+            sum_diff += fabs(pi_next[i] - pi[i]);
+            pi[i] = pi_next[i];
+        }
+    } while (sum_diff > TOLERANCE);
+}
 
 int main() {
     fastio;
 
+    int cases;
+    cin >> cases;
 
+    while (cases--) {
+        int n;
+        cin >> n;
+
+        vector<vector<double>> P(n, vector<double>(n));
+        fore(i, n) {
+            fore(j, n) {
+                cin >> P[i][j];
+            }
+            normalizeRow(P[i], n);
+        }
+
+        vector<double> pi(n);
+        fore(i, n) {
+            cin >> pi[i];
+        }
+
+        double sum = accumulate(all(pi), 0.0);
+        fore(i, n) {
+            pi[i] /= sum;
+        }
+
+        findSteadyState(P, pi, n);
+
+        fore(i, n) {
+            cout << fixed << setprecision(5) << pi[i];
+            if (i < n - 1) {
+                cout << " ";
+            }
+        }
+        cout << enl;
+    }
 
     return 0;
 }
